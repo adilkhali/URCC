400823bb0a5bf7675a916c46d8cc0ae98dca4360
Adil Khali <akhali@sqli.com>
2019-07-20 21:28:45 +0100

    Add Shops Repository

---
+++ UnitedRemote.Core/Repositories/ShopRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitedRemote.Core.Models.V1;
+
+namespace UnitedRemote.Core.Repositories
+{
+    class ShopRepository : Repository<Shop>
+    {
+        public ShopRepository(DbContext context) :base(context)
+        {
+        }
+
+        public List<Shop> GetNearbyShops(double latitude, double longitude, double searchRadious)
+        {
+            Point userLocation = new Point(latitude, longitude);
+            return _entities.Where(x => x.Location.Distance(userLocation) <= searchRadious).ToList();
+        }
+    }
+}
