318fcf0338d1d84fa7802562e80027ca87a7ae78
Adil Khali <akhali@sqli.com>
2019-07-21 01:47:38 +0100

    Add Shop view model

---
+++ UnitedRemote.Core/ViewModels/Shop/ShopViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace UnitedRemote.Core.ViewModels
+{
+    public class ShopViewModel
+    {
+
+        [NotMapped]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double latitude { get; set; }
+        [Required]
+        public double longitude { get; set; }
+    }
+}
---
+++ UnitedRemote.Core/ViewModels/Shop/UserLocationViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace UnitedRemote.Core.ViewModels.Shop
+{
+    public class UserLocationViewModel
+    {
+        [Required]
+        public double longitude { get; set; }
+        [Required]
+        public double latitude { get; set; }
+    }
+}
